30f5483592d9ffd2258716b26c3cc946ef231349
Tara Evans <tarajevans@hotmail.com>
2022-10-09 19:39:35 -0400

    Add to staging Lesson 4

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const {InjectManifest} = require('workbox-webpack-plugin');
 
   module.exports = {
     entry: './src/js/index.js',
@@ -37,6 +37,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
       })
     ]
     
