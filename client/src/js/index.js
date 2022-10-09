56f744b638b22b718c36470e4c0c657390208171
Tara Evans <tarajevans@hotmail.com>
2022-10-09 13:31:02 -0400

    created indexedDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+import { initdb } from './database.js';
 
 
 
@@ -16,4 +17,5 @@ window.addEventListener('load', function () {
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
+    initdb();
   });
