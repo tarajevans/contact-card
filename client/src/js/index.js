02153d1469a137ad0fc9c9f8a6efa637e56b133a
Tara Evans <tarajevans@hotmail.com>
2022-10-09 15:32:40 -0400

    delete functionlity

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,4 @@
 import "./form.js";
-import "./submit.js";
 // Import CSS files
 import "../css/index.css";
 
@@ -9,7 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
-import { initdb } from './database.js';
+import { initdb, getDb, postDb, deleteDb } from './database.js';
+import { fetchCards } from './card.js';
+import { toggleForm, clearForm } from './form.js';
 
 
 
@@ -18,4 +19,51 @@ window.addEventListener('load', function () {
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
     initdb();
+    getDb();
+    fetchCards();
   });
+
+    // Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+ })
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let email = document.getElementById("email").value;
+let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+if (submitBtnToUpdate == false) {
+  postDb(name, email, phone, profile);
+} else {
+
+  fetchCards();
+    // Toggles the submit button back to POST functionality
+  submitBtnToUpdate = false;
+}
+
+// Clear form
+clearForm();
+// Toggle form
+toggleForm();
+// Reload the DOM
+fetchCards();
+});
+
+window.deleteCard = (e) => {
+  // Grabs the id from the button element attached to the contact card.
+  let id = parseInt(e.id);
+  // Delete the card
+  deleteDb(id);
+  // Reload the DOM
+  fetchCards();
+};
